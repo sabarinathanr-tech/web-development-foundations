bb60e61f69c92884aad8ca3f5aa4634952fa1c99
sabarinathanr-tech <sabarinathanr.tech@gmail.com>
2026-04-06 21:37:51 +0530

    Added JavaScript variables and datatypes practice files

---
+++ Java script/Variable Datatypes/scripts.js
@@ -0,0 +1,71 @@
+console.log("\n=== VARIABLES ===");
+
+let a=10;
+let b=30;
+const c=20;
+console.log("a:",a);
+console.log("b:",b);
+
+a=40;
+console.log("after changing a becomes:",a);
+
+console.log("\n=== DATA TYPES ===");
+
+let n=12;
+let s="Hi,Nice to meet you!";
+let name="Sabarinathan";
+let msg = `${s} ${name}`;
+let bool=true;
+let empty=null;
+let undef;
+let big=1234567890098721n;
+let id=Symbol("id");
+
+console.log("n:",n);
+console.log("s:",msg);
+
+console.log("\n=== TYPE COERCION ===");
+
+console.log('"5" + 2 =', "5" + 2);
+console.log('"5" - 2 =', "5" - 2);
+console.log('true + 1 =', true + 1);
+
+
+console.log("\n=== COMPARISON ===");
+
+console.log("5 == '5' :", 5 == "5");
+console.log("5 === '5':", 5 === "5");
+
+
+console.log("\n=== TRUTHY / FALSY ===");
+
+if ("") {
+  console.log("Empty string is TRUE");
+} else {
+  console.log("Empty string is FALSE");
+}
+
+if ("hello") {
+  console.log("Non-empty string is TRUE");
+}
+
+
+console.log("\n=== ARRAY REFERENCE ===");
+
+let arr1 = [1, 2];
+let arr2 = arr1;
+
+arr2.push(3);
+
+console.log("arr1:", arr1);
+console.log("arr2:", arr2);
+
+
+console.log("\n=== CHALLENGE ===");
+
+let x = [1, 2];
+let y = x;
+
+y = [3, 4];
+
+console.log("x:", x); 
